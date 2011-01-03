678e8dc1bb66c14e0fd8dd4b483138fe5554c6e1
livermouse <livermouse@246793b4-ec7a-51e2-1e46-814b1dcfc0cb>
2011-01-03 19:06:22 +0000

    add more demo views

--- demo/scripts/demo.js
+++ demo/scripts/demo.js
@@ -4,24 +4,42 @@ function setup() {
     var views = $.playground().addGroup("views", {width: jQuest.playgroundWidth, height: jQuest.playgroundHeight});
 
     var west = new jQuest.View({
-        name: "west",
+        name: "westWall",
         group: views,
         backgroundImage: "img/views/room-wall-west.png"
     });
+    var westDetail = new jQuest.View({
+        name: "westDetail",
+        group: views,
+        backgroundImage: "img/views/room-wall-west-detail.png"
+    });
     var north = new jQuest.View({
-        name: "north",
+        name: "northWall",
         group: views,
         backgroundImage: "img/views/room-wall-north.png"
     });
+    var northDetail = new jQuest.View({
+        name: "northDetail",
+        group: views,
+        backgroundImage: "img/views/room-wall-north-detail.png"
+    });
     var east = new jQuest.View({
-        name: "east",
+        name: "eastWall",
         group: views,
         backgroundImage: "img/views/room-wall-east.png"
     });
+    var eastDetail = new jQuest.View({
+        name: "eastDetail",
+        group: views,
+        backgroundImage: "img/views/room-wall-east-detail.png"
+    });
 
-    west.setView('right', north);
-    north.setView('left', west).setView('right', east);
-    east.setView('left', north);
+    west.setView('right', north).setView('forward', westDetail);
+    westDetail.setView('backward', west);
+    north.setView('forward', northDetail).setView('left', west).setView('right', east);
+    northDetail.setView('backward', north);
+    east.setView('forward', eastDetail).setView('left', north);
+    eastDetail.setView('backward', east);
 
     window.game = new jQuest.Game(west);
 }
